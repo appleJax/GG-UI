09eee5fe20d58b28bed87a44526111c5945259be
appleJax <appleJax@users.noreply.github.com>
2018-02-05 02:32:26 -0600

    add webpack production config

---
+++ webpack.common.js
@@ -0,0 +1,65 @@
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+  resolve: {
+    alias: {
+      Actions: path.resolve(__dirname, 'src/actions/'),
+      Ajax: path.resolve(__dirname, 'src/ajax/'),
+      Components: path.resolve(__dirname, 'src/components/'),
+      Containers: path.resolve(__dirname, 'src/containers'),
+      Images: path.resolve(__dirname, 'src/static/images/'),
+      Reducers: path.resolve(__dirname, 'src/reducers/'),
+      Styles: path.resolve(__dirname, 'src/styles/'),
+      Src: path.resolve(__dirname, 'src/'),
+      UI: 'material-ui',
+      Utils: path.resolve(__dirname, 'src/utils/')
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-react',
+              '@babel/preset-env'
+            ]
+          }
+        }
+      },
+      { test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            { loader: 'css-loader' }
+          ]
+        }),
+      },
+      { test: /\.(png|jpg|jpeg)$/,
+        use: 'file-loader'
+      }
+    ]
+  },
+  plugins: [
+    new CopyWebpackPlugin([ { from: 'src/static' } ]),
+    new CleanWebpackPlugin(__dirname + '/dist'),
+    new ExtractTextPlugin('bundle.css'),
+    new HtmlWebpackPlugin({
+      template: __dirname + '/src/static/index.html',
+      inject: 'body'
+    })
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
+  }
+};
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,19 +1,17 @@
 const merge = require('webpack-merge')
+const webpack = require('webpack');
 const common = require('./webpack.common')
 
 module.exports = merge(common, {
   entry: [
     'babel-polyfill',
-    'react-hot-loader/patch',
     './src/index.js'
   ],
-  devtool: 'eval',
-  devServer: {
-    contentBase: './dist',
-    historyApiFallback: true,
-    hot: true
-  },
   plugins: [
-    new webpack.HotModuleReplacementPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+         API_URL: process.env.API_URL
+      }
+    })
   ]
 });
