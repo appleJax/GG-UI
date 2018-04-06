6a3bf1bc563fecf4ffa68377fbdbebd23cc27fea
appleJax <appleJax@users.noreply.github.com>
2018-04-06 01:46:55 -0500

    increase height of submit answer button

--- src/view/containers/LiveQuestions/styles.js
+++ src/view/containers/LiveQuestions/styles.js
@@ -56,6 +56,7 @@ export default (theme) => ({
   submitAnswerBtn: {
     background: 'rgba(0,0,0,0.1)',
     color: 'rgba(255,255,255,0.5)',
+    height: '44px',
     '&:hover': {
       background: 'rgba(0,0,0,0.2)'
     }
